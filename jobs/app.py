f51d88a25e2e566e2a38d4edc340a5010b16695c
Chris Scarry <scarrychris@gmail.com>
2018-11-30 09:31:15 -0500

    First couple changes to app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,17 @@
 #!/venv/bin/python3
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+
+PATH = ('/db/jobs.sqlite')
+
+engine = create_engine('sqlite:///jobs.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+open_connection = get_addr('_connection')
+if _connection == None:
+	set _connection == session
+	pass
 
 app = Flask(__name__) 
 
